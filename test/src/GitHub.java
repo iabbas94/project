657e51898e781dfcda7d88c08d73036fdd79e106
Ilkin Abbasov <ilkinabbasov01@gmail.com>
2021-06-23 09:26:18 -0700

    file edited

---
+++ test/src/GitHub.java
@@ -0,0 +1,15 @@
+public class GitHub {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Welcome to java " +
+                "Welcome to Github");
+    }
+
+
+
+
+
+}
